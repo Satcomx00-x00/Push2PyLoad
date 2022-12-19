86d5194774c95a9494ad1ec3696d6d7731de829d
Satcom <47340064+Satcomx00-x00@users.noreply.github.com>
2022-12-19 19:58:12 +0100

    update

--- background.js
+++ background.js
@@ -1,41 +1,49 @@
-// Listen for right-click events on the page.
-document.addEventListener("contextmenu", handleContextMenu);
+// définition de l'URL de l'API PyLoad et des identifiants d'accès
 
-function handleContextMenu(event) {
-    // Retrieve the URL of the currently highlighted tab.
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        var activeTab = tabs[0];
-        var url = activeTab.url;
+const apiUrl = 'http://172.17.0.11:8000/api/addPackage';
 
-        // Send the URL to the PyLoad API server.
-        sendUrlToPyLoad(url);
-    });
-}
+const username = 'pyload';
+const password = 'pyload';
+// ajout d'un écouteur d'événement pour détecter lorsque l'utilisateur a highlighté du texte
+document.addEventListener('mouseup', event => {
+    // récupération de l'URL highlightée
+    const selection = window.getSelection().toString().trim();
+    if (selection && isUrl(selection)) {
+        // demande du nom du package à l'utilisateur
+        const packageName = prompt('Entrez le nom du package :');
 
-function sendUrlToPyLoad(url) {
-    var pyloadApiUrl = "http://pyload.example.com/api/addPackage";
-
-    // Make a POST request to the PyLoad API.
-    fetch(pyloadApiUrl, {
-        method: "POST",
-        body: JSON.stringify({ url: url }),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-        .then(function (response) {
-            // Check the status of the response.
-            if (response.status === 200) {
-                // If the request was successful, display a notification to the user.
-                chrome.notifications.create(null, {
-                    type: "basic",
-                    iconUrl: "icon.png",
-                    title: "Send URL to PyLoad",
-                    message: "The URL was sent to the PyLoad server successfully."
-                });
-            }
+        // envoi de l'URL au serveur API PyLoad
+        fetch(apiUrl + 'addPackage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + btoa(username + ':' + password)
+            },
+            body: JSON.stringify({
+                name: packageName,
+                links: [selection]
+            })
         })
-        .catch(function (error) {
-            // TODO: Handle any errors that occur.
-        });
+            .then(response => response.json())
+            .then(result => {
+                if (result.success) {
+                    alert('Lien envoyé avec succès !');
+                } else {
+                    alert('Erreur lors de l\'envoi du lien : ' + result.error);
+                }
+            })
+            .catch(error => {
+                alert('Erreur lors de la communication avec le serveur API : ' + error);
+            });
+    }
+});
+
+// fonction pour vérifier si une chaîne de caractères est une URL valide
+function isUrl(str) {
+    try {
+        new URL(str);
+        return true;
+    } catch (error) {
+        return false;
+    }
 }
